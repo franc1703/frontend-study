0380a9d34f8c93580b216a90d937e04f47bb8eb9
Mr.uLLetIcaL <sashae12027@gmail.com>
2022-11-25 20:08:21 +0300

    изучение циклов  в JS

---
+++ иузчение frontend/JS/четвертое занятие/ch.js
@@ -0,0 +1,43 @@
+//цикл for
+// var i = 0;
+// for (; i<15; i++){
+
+// console.log(i);
+// }
+// console.log("конец работы");
+
+// var i = 25;
+// for (; i>15; i--){
+//     console.log(i)
+// }
+
+// var i = 0;
+// for(; i<60; i+=10){
+//     console.log(i);
+// }
+
+// var i = 0;
+// var j = 0;
+// for(; i<60; j<30, i+=10, j+=10){
+//     console.log(i+j);
+// }
+
+// for (let i = 1; i <= 5; i++) {
+//     for (let g = 1; i <= 5; g++) {
+//         console.log(i * g);
+//     }
+// }
+//цикл while
+// let i = 1;
+// while (i <= 6) {
+//     console.log(i);
+//     i++;
+// }
+ //цикл do while
+
+
+ var i = 1;
+ do{
+    console.log(i);
+        i++;
+ }while(i<=10)
